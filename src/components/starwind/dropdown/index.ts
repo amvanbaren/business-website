117f7ff7e4dac86fb6f722c4943954de616468cb
amvanbaren <amvanbaren@hotmail.com>
2026-02-13 01:21:11 +0200

    Format code

--- src/components/starwind/dropdown/index.ts
+++ src/components/starwind/dropdown/index.ts
@@ -2,7 +2,9 @@ import Dropdown from "./Dropdown.astro";
 import DropdownContent, { dropdownContent } from "./DropdownContent.astro";
 import DropdownItem, { dropdownItem } from "./DropdownItem.astro";
 import DropdownLabel, { dropdownLabel } from "./DropdownLabel.astro";
-import DropdownSeparator, { dropdownSeparator } from "./DropdownSeparator.astro";
+import DropdownSeparator, {
+  dropdownSeparator,
+} from "./DropdownSeparator.astro";
 import DropdownTrigger, { dropdownTrigger } from "./DropdownTrigger.astro";
 
 const DropdownVariants = {
