dc7acd11aceb2e17bf05a875d83dc0addd5736bb
amvanbaren <amvanbaren@hotmail.com>
2026-03-19 11:36:43 +0200

    Bump astro
    - Bump astro to v6
    - Fix yarn
    - Change assets binding

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://business-website-ckb.pages.dev",
-
+  output: 'server',
   adapter: cloudflare({
+    assets: {
+      binding: "STATIC_ASSETS"
+    },
     platformProxy: {
       enabled: true,
     },
