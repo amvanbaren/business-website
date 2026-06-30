ca6709a51925b74caf0c54b1daf9a73306d243e4
amvanbaren <amvanbaren@hotmail.com>
2026-06-30 23:15:13 +0300

    remove @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
-// @ts-check
 import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://precies.software",
-
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-
-    imageService: { build: 'compile', runtime: 'cloudflare-binding' },
-  }),
-
   vite: {
       plugins: [tailwindcss()],
 	},
-
   integrations: [sitemap()],
 });
